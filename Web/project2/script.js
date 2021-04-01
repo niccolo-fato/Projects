7be74b9c7fb6bae4bebbbbabd5684900d07b7435
Niccolò Fato <niccolofato13@gmail.com>
2021-04-01 12:35:38 +0200

    Create script.js

---
+++ Web/project2/script.js
@@ -0,0 +1,7 @@
+function s() {
+    var time = document.getElementById('time').value;
+    var speed = document.getElementById('speed').value;
+    var spazio = 0;
+    spazio = time * speed;
+    alert(spazio+"km");
+}
