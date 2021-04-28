c4336638c2f7bc953faa43c450169ea25a3c7b26
Niccolo Fato <niccolofato13@gmail.com>
2021-04-28 15:29:33 +0200

    Update pointer1.cpp

--- C++/4BI/Pointers/pointer1.cpp
+++ C++/4BI/Pointers/pointer1.cpp
@@ -7,45 +7,53 @@ opzioni: 1.arrivo di un paziente; 2.estrazione del paziente dalla coda;
 #include <iostream>
 #include <string>
 using namespace std;
-struct patient{
+struct patient
+{
     string name;
     int num;
-    struct patient *next; // puntatore al successivo
+    struct patient* next; // puntatore al successivo
 };
-class patients{
-    patient *head; // puntatore al primo elemento della lista
-    patient *end;  // puntatore all'ultimo elemento della lista
+class patients
+{
+    patient* head; // puntatore al primo elemento della lista
+    patient* end;  // puntatore all'ultimo elemento della lista
     int num;       // primo numero distribuito
 public:
-    patients(){ // costruttore
+    patients()
+    { // costruttore
         head = NULL;
         end = NULL; // inizializzazione della coda e testa a vuoto
         num = 1;
     }
-    void push(){ // estrazione del paziente in coda
+    void push()
+    { // estrazione del paziente in coda
         int choice = 0;
-        patient *p;
+        patient* p;
         p = new (patient);
+        cout << "\t\t Menu scelta paziente:\n";
         cout << "Inserisci 1 se il paziente presenta delle disabilita' o in stato "
-                "di gravidanza, \n"
-             << "Inserisci 2 se non rispetta le condizioni dell'opzione 1 \n"
-             << "Scelta:";
+            "di gravidanza, \n"
+            << "Inserisci 2 se non rispetta le condizioni dell'opzione 1, \n"
+            << "Scelta:";
         cin >> choice;
         switch (choice)
         {
-        case 1:{
+        case 1:
+        {
             cout << "Inserire il nome del paziente:";
             cin >> p->name;
-            p->num = num++;
-            cout << "Il paziente " << p->name << " ha il numero " << p->num << "\n";
+            p->num = 0;
+            cout << "Il paziente " << p->name << " ha ricevuto un permesso speciale per saltare la fila "
+                << "\n";
             if (head == NULL)   // controllo se il paziente inserito sia il primo
                 head = end = p; // inserimento del primo paziente
-            else{
+            else
+            {
                 p->next = head;
                 head = p;
             }
         }
-            break;
+        break;
         case 2:
             cout << "Inserire il nome del paziente:";
             cin >> p->name;
@@ -54,7 +62,8 @@ public:
             p->next = NULL;
             if (head == NULL)   // controllo se il paziente inserito sia il primo
                 head = end = p; // inserimento del primo paziente
-            else{
+            else
+            {
                 end->next = p;
                 end = p;
             }
@@ -63,15 +72,18 @@ public:
     }
     void pop()
     { // estrazione del primo paziente in coda
-        patient *p;
+        patient* p;
         if (head == NULL)
             cout << "ERROR==>Non ci sono pazienti in attesa\n";
         else
         {
             p = head;
             head = p->next;
-            cout << "Il prossimo da visitare e' " << p->name << " e ha il  numero "
-                 << p->num << "\n";
+            if (p->num == 0)
+                cout << "Il prossimo da visitare e' " << p->name << " che ha saltato la fila per un'urgenza" << "\n";
+            else
+                cout << "Il prossimo da visitare e' " << p->name << " e ha il  numero "
+                << p->num << "\n";
             if (head == NULL)
                 end = NULL;
             delete p;
@@ -79,7 +91,7 @@ public:
     }
     void print()
     { // stampa dei pazienti in attesa
-        patient *p;
+        patient* p;
         if (head == NULL) // controllo se sono stati inseriti dei pazienti
             cout << "ERROR==>Non ci sono pazienti in attesa\n";
         else
@@ -87,8 +99,18 @@ public:
             cout << "\t\tELENCO PAZIENTI IN ATTESA\n";
             for (p = head; p != NULL; p = p->next)
             {
-                cout << "Nome:\t\t Numero:\n";
-                cout << p->name << "\t\t " << p->num << "\n";
+                if (p->num == 0)
+                {
+                    cout << "Nome:\t\t Numero:\n";
+                    cout << p->name << "\t\t "
+                        << "!Dev'essere visitato con urgenza!"
+                        << "\n";
+                }
+                else
+                {
+                    cout << "Nome:\t\t Numero:\n";
+                    cout << p->name << "\t\t " << p->num << "\n";
+                }
             }
         }
     }
@@ -97,15 +119,17 @@ int main()
 {
     int choice = 0;
     patients office;
-    do{ // menu scelta
+    do
+    { // menu scelta
         cout << "\t\t Menu scelta:\n";
         cout << "0-Uscire dal programma,\n"
-             << "1-Inserire paziente,\n"
-             << "2-Estrai paziente,\n"
-             << "3-Stampa elenco pazienti in attesa,\n"
-             << "Scelta:";
+            << "1-Inserire paziente,\n"
+            << "2-Estrai paziente,\n"
+            << "3-Stampa elenco pazienti in attesa,\n"
+            << "Scelta:";
         cin >> choice;
-        switch (choice){
+        switch (choice)
+        {
         case 1:
             office.push();
             break;
